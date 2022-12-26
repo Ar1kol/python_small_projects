3527853448fd7ebf780afea7e10a18a17f8224ca
Ar1kol <108225943+Ar1kol@users.noreply.github.com>
2022-12-26 12:34:47 +0200

    update web-scraping

--- web_scraping.py
+++ web_scraping.py
@@ -2,3 +2,13 @@ import requests
 from bs4 import BeautifulSoup
 
 url = "https://www.codewithtomi.com/"
+request = requests.get(url)
+print(request)
+
+soup = BeautifulSoup(request.content, "lxml")
+
+titles = soup.find_all("h2", {"class": "post-title"})
+
+# print(title)
+for title in titles:
+    print(title.getText(), end="")
